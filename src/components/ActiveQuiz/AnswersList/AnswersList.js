dfec8eae31a28ac77060b2f6aa7b5aa2e803eef5
pyankovit <pyankov.it@gmail.com>
2019-03-07 15:07:12 +0200

    new

---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './AnswersList.css';
+
+const AnswersList = props =>(
+    <ul className={classes.AnswersList}>
+    {props.answers.map((answer, index)=>{
+        
+    })}
+    <li></li>
+    
+    </ul>
+
+)
+
+
+export default AnswersList
